dceecf073d4df83eea4c57aee69389486c014a6a
achooie <achooie5771@gmail.com>
2016-12-25 10:49:24 -0600

    Updated random selector to remove empty lines from ls.

--- scripts/rand_selector.py
+++ scripts/rand_selector.py
@@ -8,12 +8,11 @@ import sys
 import re
 
 movies = sys.stdin.read().split("\n")
-pattern = re.compile('.*:.*', re.IGNORECASE)
-movies.remove("")
+pattern = re.compile('(.*:.*)|(^$)', re.IGNORECASE)
 
 if len(movies) < 1:
     print "\nUsage:\n\t<set of line-delimited items> | python2 \
-rand_selector.py\n"
+rand_selector.py [# of choices]\n"
 else:
     filteredMovies = filter(lambda i: not pattern.search(i), movies)
     print ""
