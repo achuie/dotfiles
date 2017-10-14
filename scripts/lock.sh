837f7ca4851882ff44f946ffa25f7c855fb99d1c
achooie <achooie5771@gmail.com>
2017-10-15 01:39:01 +0900

    Updated lock.sh and i3 config to use i3lock; more configs for other programs.

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,8 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
-xset +dpms
-slock
-xset -dpms
-xset s off
+revert() {
+    xset dpms 0 0 0
+}
 
-exit 0
+trap revert HUP INT TERM
+xset +dpms dpms 15 15 15
+i3lock -n -e -i /home/achuie/.lockscreen.png
+revert
