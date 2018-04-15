063d2fc1d5fa6ac24dc0b481da503cc9fa0157bd
achooie <achooie5771@gmail.com>
2018-04-15 16:45:36 +0900

    Updates from the new setup.

--- scripts/update_freedns_sentinel_external.sh
+++ scripts/update_freedns_sentinel_external.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/usr/bin/wget --read-timeout=0 - http://freedns.afraid.org/dynamic/update.php?bzFzY0o3SVVtVHFnMk9OQmt4c3BTTmlkOjExODQ3NDc1
-rm update.php?bzFzY0o3SVVtVHFnMk9OQmt4c3BTTmlkOjExODQ3NDc1
+/usr/bin/wget --read-timeout=0 - http://freedns.afraid.org/dynamic/update.php?bzFzY0o3SVVtVHFnMk9OQmt4c3BTTmlkOjE3MjU5MTA1
+rm update.php?bzFzY0o3SVVtVHFnMk9OQmt4c3BTTmlkOjE3MjU5MTA1
