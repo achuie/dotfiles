194a8f7019e50c9fb61928bf4c648a5e5ff8f4b9
achooie <achooie5771@gmail.com>
2018-08-15 23:55:16 +0900

    Switched to pulseaudio; updated mpd config for pulse and tried to fix skipping issue; added Rush to library; added mpv binding to qutebrowser; added fcitx.

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -10,21 +10,64 @@
 # Type: Bool
 c.auto_save.session = True
 
-# The directory to save downloads to. If unset, a sensible os-specific
+# Enable JavaScript.
+# Type: Bool
+config.set('content.javascript.enabled', True, 'file://*')
+
+# Enable JavaScript.
+# Type: Bool
+config.set('content.javascript.enabled', True, 'chrome://*/*')
+
+# Enable JavaScript.
+# Type: Bool
+config.set('content.javascript.enabled', True, 'qute://*/*')
+
+# Directory to save downloads to. If unset, a sensible OS-specific
 # default is used.
 # Type: Directory
 c.downloads.location.directory = '~/downloads'
 
-# Bindings for normal mode
-#config.bind('<ctrl+pgdown>', 'tab-prev')
-#config.bind('<ctrl+pgup>', 'tab-focus')
-#config.bind('J', 'tab-prev')
-#config.bind('K', 'tab-focus')
-
-c.fonts.hints = "bold 7pt monospace"
-
+# Open new tabs (middleclick/ctrl+click) in the background.
+# Type: Bool
 c.tabs.background = True
-c.tabs.position = "left"
-c.tabs.width = "12%"
 
-c.url.default_page = "about:blank"
+# Position of the tab bar.
+# Type: Position
+# Valid values:
+#   - top
+#   - bottom
+#   - left
+#   - right
+c.tabs.position = 'left'
+
+# Width (in pixels or as percentage of the window) of the tab bar if
+# it's vertical.
+# Type: PercOrInt
+c.tabs.width = '12%'
+
+# Page to open if :open -t/-b/-w is used without URL. Use `about:blank`
+# for a blank page.
+# Type: FuzzyUrl
+c.url.default_page = 'about:blank'
+
+# Search engines which can be used via the address bar. Maps a search
+# engine name (such as `DEFAULT`, or `ddg`) to a URL with a `{}`
+# placeholder. The placeholder will be replaced by the search term, use
+# `{{` and `}}` for literal `{`/`}` signs. The search engine named
+# `DEFAULT` is used when `url.auto_search` is turned on and something
+# else than a URL was entered to be opened. Other search engines can be
+# used by prepending the search engine name to the search term, e.g.
+# `:open google qutebrowser`.
+# Type: Dict
+c.url.searchengines = {'DEFAULT': 'https://searx.me/?q={}&categories=general&language=en-US'}
+
+# Page(s) to open at the start.
+# Type: List of FuzzyUrl, or FuzzyUrl
+c.url.start_pages = 'https://searx.me'
+
+# Font used for the hints.
+# Type: Font
+c.fonts.hints = 'bold 7pt monospace'
+
+# Bindings
+config.bind("\m", "hint links spawn mpv {hint-url}")
