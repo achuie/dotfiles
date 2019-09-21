3cf04144df5bdf050c7b402db60392bbb1ac4684
achooie <achuie5771@gmail.com>
2019-09-22 01:51:46 +0900

    Show previous exit status.

--- .zsh/prompts/achuie.zsh
+++ .zsh/prompts/achuie.zsh
@@ -1,20 +1,29 @@
 achuie='[%(!.%F{red}.%F{blue})%n@%m%f %1~${vcs_info_msg_0_}]%# '
 achuievi='[%F{yellow}%n@%m%f %1~${vcs_info_msg_0_}]%# '
 
-function set_prompt() {
+function set_prompt {
     case ${KEYMAP} in
         (vicmd) PS1=$achuievi ;;
         (main|viins) PS1=$achuie ;;
         (*) PS1=$achuie ;;
     esac
+
+    RPS1='%F{red}%(?..(%?%))%f'
 }
 
-function prompt_achuie_precmd() {
+function prompt_achuie_precmd {
     vcs_info
     set_prompt
 }
 
-function prompt_achuie_setup() {
+function zle-line-init zle-keymap-select {
+    set_prompt
+    zle reset-prompt
+}
+zle -N zle-line-init
+zle -N zle-keymap-select
+
+function prompt_achuie_setup {
     autoload -Uz vcs_info
     zstyle ':vcs_info:*' enable git
 
@@ -26,14 +35,6 @@ function prompt_achuie_setup() {
     zstyle ':vcs_info:*' formats " %F{green}(%b)%c%m%f"
 
     precmd_functions+=(prompt_achuie_precmd)
-    set_prompt
 }
-
-function zle-line-init zle-keymap-select {
-    set_prompt
-    zle reset-prompt
-}
-zle -N zle-line-init
-zle -N zle-keymap-select
 
 prompt_achuie_setup "$@"
