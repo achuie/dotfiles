2ae198e1b4fb8abf7ae6c16023224ac7e3dc0b37
achooie <achuie5771@gmail.com>
2019-09-21 18:38:04 +0900

    Fixed formatting problems.

--- .zsh/prompts/achuie.zsh
+++ .zsh/prompts/achuie.zsh
@@ -10,12 +10,11 @@ function prompt_achuie_setup() {
     zstyle ':vcs_info:*' enable git
 
     zstyle ':vcs_info:*' check-for-changes true
-    zstyle ':vcs_info:*' unstagedstr '!'
-    zstyle ':vcs_info:*' stagedstr '*'
-    zstyle ':vcs_info:*' actionformats '%F{green}(%b|%a)%c%u%f'
-    zstyle '"vcs_info:*' formats '%F{green}(%b)%c%u%f'
+    zstyle ':vcs_info:*' unstagedstr "!"
+    zstyle ':vcs_info:*' stagedstr "*"
+    zstyle ':vcs_info:*' actionformats " %F{green}(%b|%a)%c%u%f"
+    zstyle ':vcs_info:*' formats " %F{green}(%b)%c%u%f"
 
-    vcs_info
     precmd_functions+=(prompt_achuie_precmd)
     PS1=$achuie
 }
