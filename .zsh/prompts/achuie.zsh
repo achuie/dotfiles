9171455d29021568d3c4dc757c9e81a53a1e999c
achooie <achuie5771@gmail.com>
2019-09-21 23:57:03 +0900

    Vi mode indication.

--- .zsh/prompts/achuie.zsh
+++ .zsh/prompts/achuie.zsh
@@ -1,22 +1,39 @@
 achuie='[%(!.%F{red}.%F{blue})%n@%m%f %1~${vcs_info_msg_0_}]%# '
+achuievi='[%F{yellow}%n@%m%f %1~${vcs_info_msg_0_}]%# '
+
+function set_prompt() {
+    case ${KEYMAP} in
+        (vicmd) PS1=$achuievi ;;
+        (main|viins) PS1=$achuie ;;
+        (*) PS1=$achuie ;;
+    esac
+}
 
 function prompt_achuie_precmd() {
     vcs_info
-    PS1=$achuie
+    set_prompt
 }
 
 function prompt_achuie_setup() {
     autoload -Uz vcs_info
     zstyle ':vcs_info:*' enable git
 
-    zstyle ':vcs_info:*' check-for-changes true
-    zstyle ':vcs_info:*' unstagedstr "!"
-    zstyle ':vcs_info:*' stagedstr "*"
-    zstyle ':vcs_info:*' actionformats " %F{green}(%b|%a)%c%u%f"
-    zstyle ':vcs_info:*' formats " %F{green}(%b)%c%u%f"
+    zstyle ':vcs_info:*' check-for-staged-changes true
+    zstyle ':vcs_info:*' stagedstr "+"
+    zstyle ':vcs_info:*' patch-format "%c"
+    zstyle ':vcs_info:*' nopatch-format "%c"
+    zstyle ':vcs_info:*' actionformats " %F{green}(%b|%a)%c%m%f"
+    zstyle ':vcs_info:*' formats " %F{green}(%b)%c%m%f"
 
     precmd_functions+=(prompt_achuie_precmd)
-    PS1=$achuie
+    set_prompt
 }
+
+function zle-line-init zle-keymap-select {
+    set_prompt
+    zle reset-prompt
+}
+zle -N zle-line-init
+zle -N zle-keymap-select
 
 prompt_achuie_setup "$@"
