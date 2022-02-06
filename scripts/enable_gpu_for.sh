8bbaf926639d3e5b6a0e0158d147e95ad8298577
achuie <achuie@protonmail.com>
2022-02-06 12:00:28 +0900

    Add usage.

--- scripts/enable_gpu_for.sh
+++ scripts/enable_gpu_for.sh
@@ -1,7 +1,25 @@
 #!/usr/bin/env bash
 
+function usage() {
+    cat <<EOF
+$(basename $0)
+Run Electron- or Chromium-based programs with options to explicitly enable GPU
+acceleration.
+
+USAGE:
+    $(basename $0) <PROGRAM>
+
+ARGS:
+    PROGRAM     The program to run
+
+EOF
+}
 if [ $# -lt 1 ]; then
+    usage
     exit 1
+elif [ "$1" == "-h" -o "$1" == "--help" ]; then
+    usage
+    exit 0
 fi
 
 SANDBOX=""
