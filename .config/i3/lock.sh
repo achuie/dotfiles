9279bcc1a126a3083397c84e13bab26e60346db0
achuie <achuie@protonmail.com>
2022-04-10 19:27:37 +0900

    Add blurred region for readability.

--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -1,29 +1,41 @@
 #!/usr/bin/env bash
 
 revert() {
-    xset dpms 0 0 0
-    xset -dpms
+  xset dpms 0 0 0
+  xset -dpms
 }
 
 trap revert HUP INT TERM
 xset +dpms dpms 15 15 15
 
-if [ ! -e "${HOME}/.lockscreen.png" ]; then
-    wall="$(tail -n 1 ${HOME}/.fehbg | sed "s/[^']*'//" | sed "s/' //")"
-    convert "$wall" -resize 2256x2256 -blur 0x48 "${HOME}/.lockscreen.png"
+if [ ! -e "${HOME}/.lockscreen.png" ] || [ "$1" == "-r" ]; then
+  wall="$(tail -n 1 ${HOME}/.fehbg | sed "s/[^']*'//" | sed "s/' //")"
+
+  rectX=85
+  rectY=1220
+  rectWidth=610
+  rectHeight=200
+
+  convert "$wall" -resize 2256x2256 -fill "#32344abb" \
+    -draw "rectangle ${rectX},${rectY},$(($rectX+$rectWidth)),$(($rectY+$rectHeight))" \
+    -region "${rectWidth}x${rectHeight}+${rectX}+${rectY}" \
+    -blur 0x8 "${HOME}/.lockscreen.png"
 fi
 
 i3lock --nofork --ignore-empty-password \
-    --radius 40 --ring-width 10 --indicator --ind-pos="550:y+h-180" \
-    --color=000000ff \
-    --inside-color=15151800 --ring-color=f4f4f4f0 --line-color=e8e8e800 --keyhl-color=181818a0 \
-    --ringver-color=f4f4f4ff --insidever-color=30303000 \
-    --ringwrong-color=f4f4f4f0 --insidewrong-color=404040a8 \
-    --verif-text=" " --wrong-text=" " \
-    --verif-color=00000000 --wrong-color=00000000 \
-    --clock --time-pos="ix-250:iy+5" --date-pos="ix-250:ty+60" \
-    --time-color=f4f4f4f0 --time-size=80 --time-str="%H:%M:%S" \
-    --date-color=f4f4f4f0 --date-size=45 --date-str="%a %Y-%m-%d" \
-    -i "${HOME}/.lockscreen.png"
+  --radius 60 --ring-width 15 --indicator --ind-pos="608:y+h-185" \
+  --color=00000088 \
+  --inside-color=32344a88 --ring-color=ffffffff --line-uses-ring \
+  --keyhl-color=7aa2f7ff --bshl-color=444b6aff --separator-color=00000000 \
+  --ringver-color=75c1eeff --insidever-color=7dcfff88 \
+  --ringwrong-color=ea4e6bff --insidewrong-color=ff426688 \
+  --verif-text="..." --wrong-text="!" --noinput-text="---" \
+  --verif-color=ffffffff --wrong-color=ffffffff \
+  --time-font="Fira Code" --date-font="Fira Code" --layout-font="Fira Code" \
+  --time-align 1 --date-align 1 \
+  --force-clock --time-pos="ix-513:iy+20" --date-pos="ix-503:ty+60" \
+  --time-color=ffffffff --time-size=135 --time-str="%H:%M" \
+  --date-color=ffffffff --date-size=45 --date-str="%a %Y-%m-%d" \
+  -i "${HOME}/.lockscreen.png"
 
 revert
