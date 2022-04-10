5891e271c4008b53de0d9f771f76dedd4fd75253
achuie <achuie@protonmail.com>
2022-04-10 19:24:09 +0900

    Use Fira Code.

--- .config/i3/dyn_tags.sh
+++ .config/i3/dyn_tags.sh
@@ -6,4 +6,4 @@ I3MSG=$(command -v i3-msg) || exit 1
 JQ=$(command -v jq) || exit 2
 
 $I3MSG workspace $($I3MSG -t get_workspaces | $JQ -M '.[] | .name' \
-    | tr -d '"' | sort -u | dmenu)
+    | tr -d '"' | sort -u | dmenu --font "Fira Code")
