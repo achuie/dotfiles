22d536b773c8cea1e055526d3b81633f212094b8
achuie <31836492+achuie@users.noreply.github.com>
2022-09-01 15:35:48 +0900

    Wezterm config.

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,53 @@
+local wezterm = require 'wezterm'
+
+return {
+  hide_tab_bar_if_only_one_tab = true,
+
+  font = wezterm.font_with_fallback {
+    'Fira Code Custom',
+    'Iosevka Custom Extended',
+  },
+  font_size = 9,
+  font_rules = {
+    {
+      italic = true,
+      font = wezterm.font('Iosevka Custom Extended', { italic = true }),
+    },
+    {
+      italic = true,
+      intensity = 'Bold',
+      font = wezterm.font('Iosevka Custom Extended', { italic = true }),
+    },
+  },
+
+  color_scheme = 'tokyo-night-storm',
+  color_schemes = {
+    ['tokyo-night-storm'] = {
+      foreground = '#c9d3ff',
+      background = 'rgba(36, 40, 58, 229.5)', -- '#24283b',
+      -- cursor_fg = '#24283b',
+      -- cursor_bg = '#c9d3ff',
+      selection_bg = '#2d4370',
+      ansi = {
+        '#32344a',
+        '#ea4e6b',
+        '#9ece6a',
+        '#ff9e64',
+        '#7aa2f7',
+        '#ad8ee6',
+        '#75c1ee',
+        '#b6b9cc',
+      },
+      brights = {
+        '#444b6a',
+        '#ff4266',
+        '#ace173',
+        '#e0af68',
+        '#9abaff',
+        '#bb9af7',
+        '#7dcfff',
+        '#cbcff5',
+      },
+    },
+  },
+}
