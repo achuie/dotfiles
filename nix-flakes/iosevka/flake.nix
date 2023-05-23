8965a25aeea71021a020072829630cb2c5f530e8
achuie <31836492+achuie@users.noreply.github.com>
2023-05-23 23:07:29 +0900

    Update Iosevka (#4)
    * Update to Iosevka 23; disable ligatures.
    
    * Update flake.lock
    
    * Remove flake-utils.

--- nix-flakes/iosevka/flake.nix
+++ nix-flakes/iosevka/flake.nix
@@ -3,71 +3,83 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    npmlock2nix-git = {
-      url = "github:nix-community/npmlock2nix";
-      flake = false;
-    };
-    iosevka = {
-      url = "github:be5invis/Iosevka/v15.5.0";
-      flake = false;
-    };
-    utils.url = "github:numtide/flake-utils";
+
+    npmlock2nix.url = "github:nix-community/npmlock2nix";
+    npmlock2nix.flake = false;
+
+    iosevka.url = "github:be5invis/Iosevka/v23.0.0";
+    iosevka.flake = false;
   };
 
-  outputs = { self, nixpkgs, utils, npmlock2nix-git, iosevka }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        npmlock2nix = pkgs.callPackage npmlock2nix-git {};
-        writeText = nixpkgs.legacyPackages.${system}.writeText;
-        customBuildConf = writeText "private-build-plans.toml" ''
-          [buildPlans.iosevka-custom]
-          family = "Iosevka Custom"
-          spacing = "normal"
-          serifs = "sans"
-          no-cv-ss = true
+  outputs = { self, nixpkgs, npmlock2nix, iosevka }:
+    let
+      forAllSystems = f:
+        nixpkgs.lib.genAttrs [ "x86_64-linux" ] (system:
+          f (import nixpkgs {
+            inherit system;
+            overlays = [ self.overlay ];
+          })
+        );
+    in
+    {
+      overlay = final: prev:
+        let
+          npml2n = (final.callPackage npmlock2nix { }).v2;
+          customBuildConf = final.writeText "private-build-plans.toml" ''
+            [buildPlans.iosevka-custom]
+            family = "Iosevka Custom"
+            spacing = "normal"
+            serifs = "sans"
+            no-cv-ss = true
+            export-glyph-names = false
+            no-ligation = true
 
-          [buildPlans.iosevka-custom.variants.design]
-          capital-q = "straight"
-          g = "double-storey"
-          l = "tailed-serifed"
-          one = "base"
-          number-sign = "slanted"
-          at = "fourfold-tall"
+            [buildPlans.iosevka-custom.variants.design]
+            capital-q = "straight"
+            g = "double-storey"
+            l = "tailed-serifed"
+            one = "base"
+            number-sign = "slanted"
+            at = "fourfold-tall"
 
-          [buildPlans.iosevka-custom.variants.italic]
-          capital-q = "open-swash"
-          g = "single-storey-flat-hook-serifless"
-          y = "cursive-flat-hook"
+            [buildPlans.iosevka-custom.variants.italic]
+            capital-q = "open-swash"
+            g = "single-storey-flat-hook-serifless"
+            y = "cursive-flat-hook"
 
-          [buildPlans.iosevka-custom.slopes.upright]
-          angle = 0
-          shape = "upright"
-          menu = "upright"
-          css = "normal"
+            [buildPlans.iosevka-custom.slopes.upright]
+            angle = 0
+            shape = "upright"
+            menu = "upright"
+            css = "normal"
 
-          [buildPlans.iosevka-custom.slopes.italic]
-          angle = 9.4
-          shape = "italic"
-          menu = "italic"
-          css = "italic"
-        '';
-      in {
-        defaultPackage = npmlock2nix.build {
-          src = iosevka;
-
-          buildInputs = [ pkgs.ttfautohint ];
-          buildCommands = [
-            "cp ${customBuildConf} ./private-build-plans.toml"
-            "npm run build -- ttf::iosevka-custom"
-          ];
-
-          installPhase = ''
-            mkdir -p $out/share/fonts/truetype
-            cp -r dist/iosevka-custom $out/share/fonts/truetype
+            [buildPlans.iosevka-custom.slopes.italic]
+            angle = 9.4
+            shape = "italic"
+            menu = "italic"
+            css = "italic"
           '';
+        in
+        {
+          iosevka-custom = npml2n.build {
+            nodejs = final.nodejs;
+            src = iosevka;
+
+            buildInputs = [ final.ttfautohint ];
+            buildCommands = [
+              "cp ${customBuildConf} ./private-build-plans.toml"
+              "npm run build -- ttf::iosevka-custom"
+            ];
+
+            installPhase = ''
+              mkdir -p $out/share/fonts/truetype
+              cp -r dist/iosevka-custom $out/share/fonts/truetype
+            '';
+          };
         };
 
-        formatter = pkgs.nixfmt;
-      });
+      packages = forAllSystems (pkgs: { default = pkgs.iosevka-custom; });
+
+      formatter = forAllSystems (pkgs: pkgs.nixpkgs-fmt);
+    };
 }
