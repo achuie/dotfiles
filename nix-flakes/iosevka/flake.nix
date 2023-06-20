3d09007a0790f7284425f75fa6706afd3b81bdcf
achuie <achuie@protonmail.com>
2023-06-21 00:57:57 +0900

    Use customizable nixpkgs iosevka.

--- nix-flakes/iosevka/flake.nix
+++ nix-flakes/iosevka/flake.nix
@@ -1,84 +1,56 @@
 {
   description = "Customized Iosevka";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
-    npmlock2nix.url = "github:nix-community/npmlock2nix";
-    npmlock2nix.flake = false;
-
-    iosevka.url = "github:be5invis/Iosevka/v23.0.0";
-    iosevka.flake = false;
-  };
-
-  outputs = { self, nixpkgs, npmlock2nix, iosevka }:
+  outputs = { self, nixpkgs }:
     let
       forAllSystems = f:
-        nixpkgs.lib.genAttrs [ "x86_64-linux" ] (system:
-          f (import nixpkgs {
-            inherit system;
-            overlays = [ self.overlay ];
-          })
-        );
+        nixpkgs.lib.genAttrs [ "x86_64-linux" ] (system: f nixpkgs.legacyPackages.${system});
     in
     {
-      overlay = final: prev:
-        let
-          npml2n = (final.callPackage npmlock2nix { }).v2;
-          customBuildConf = final.writeText "private-build-plans.toml" ''
-            [buildPlans.iosevka-custom]
-            family = "Iosevka Custom"
-            spacing = "term"
-            serifs = "sans"
-            no-cv-ss = true
-            export-glyph-names = false
-            no-ligation = true
-
-            [buildPlans.iosevka-custom.variants.design]
-            capital-q = "straight"
-            g = "double-storey"
-            l = "tailed-serifed"
-            one = "base"
-            number-sign = "slanted"
-            at = "fourfold-tall"
-
-            [buildPlans.iosevka-custom.variants.italic]
-            capital-q = "open-swash"
-            g = "single-storey-flat-hook-serifless"
-            y = "cursive-flat-hook"
-
-            [buildPlans.iosevka-custom.slopes.upright]
-            angle = 0
-            shape = "upright"
-            menu = "upright"
-            css = "normal"
-
-            [buildPlans.iosevka-custom.slopes.italic]
-            angle = 9.4
-            shape = "italic"
-            menu = "italic"
-            css = "italic"
-          '';
-        in
+      packages = forAllSystems (pkgs:
         {
-          iosevka-custom = npml2n.build {
-            nodejs = final.nodejs;
-            src = iosevka;
-
-            buildInputs = [ final.ttfautohint ];
-            buildCommands = [
-              "cp ${customBuildConf} ./private-build-plans.toml"
-              "npm run build -- ttf::iosevka-custom"
-            ];
-
-            installPhase = ''
-              mkdir -p $out/share/fonts/truetype
-              cp -r dist/iosevka-custom $out/share/fonts/truetype
-            '';
+          default = pkgs.iosevka.override {
+            privateBuildPlan = {
+              family = "Iosevka Custom";
+              spacing = "term";
+              serifs = "sans";
+              no-cv-ss = true;
+              export-glyph-names = false;
+              variants = {
+                design = {
+                  capital-q = "straight";
+                  g = "double-storey";
+                  l = "tailed-serifed";
+                  one = "base";
+                  number-sign = "slanted";
+                  at = "fourfold-tall";
+                };
+                italic = {
+                  capital-q = "open-swash";
+                  g = "single-storey-flat-hook-serifless";
+                  y = "cursive-flat-hook";
+                };
+              };
+              slopes = {
+                upright = {
+                  angle = 0;
+                  shape = "upright";
+                  menu = "upright";
+                  css = "normal";
+                };
+                italic = {
+                  angle = 9.4;
+                  shape = "italic";
+                  menu = "italic";
+                  css = "italic";
+                };
+              };
+            };
+            set = "custom";
           };
-        };
-
-      packages = forAllSystems (pkgs: { default = pkgs.iosevka-custom; });
+        });
 
       formatter = forAllSystems (pkgs: pkgs.nixpkgs-fmt);
     };
