758aa2f12f58238386255f8db04a12eca75eb3d5
achuie <31836492+achuie@users.noreply.github.com>
2023-10-06 12:47:06 +0900

    Account for different ligature block endings; open file exclusively for writing to fix length difference bug.

--- nix-flakes/firacode/remove_specific_ligatures.py
+++ nix-flakes/firacode/remove_specific_ligatures.py
@@ -3,40 +3,53 @@
 import sys
 
 def find_calt(fp):
+    fp.seek(0)
     prev_pos = fp.tell()
     line = fp.readline()
-    count = 0
-    while line.strip("\n") != "name = calt;":
-        prev_pos = fp.tell()
+    line_num = 0
+    while line.strip("\n") != "name = calt;" and line != "":
+        prev_pos = fp.tell() - len(line)
         line = fp.readline()
-        count += 1
+        line_num += 1
 
-    fp.seek(prev_pos)
+    fp.seek(prev_pos, 0)
     calt_line = fp.readline()
-    fp.seek(0)
 
-    return count, prev_pos, calt_line, fp.readlines()
+    # Previous line number since that's the calt code line.
+    return line_num - 1, prev_pos, calt_line
 
 if __name__ == "__main__":
     args = sys.argv[1:]
     with open("FiraCode.glyphs", "r") as f:
-        count, pos, calt_line, lines = find_calt(f)
+        lines = f.readlines()
+        line_num, pos, calt_line = find_calt(f)
 
     for lig in args:
         print(f" Scanning for {lig}...")
+        start = None
         try:
-            start = calt_line.index(f"lookup {lig}")
-            ending = lig + r";\012\012"
-            end = calt_line.index(ending) + len(ending)
-
-            print(f"  Removing {lig}")
-            calt_line = calt_line[:start] + calt_line[end:]
+            starting = f"lookup {lig}" + r" {\012"
+            start = calt_line.index(starting) + len(starting)
         except ValueError as e:
-            print(e)
+            print(f"Error: could not find start: {e}")
+            exit(1)
+
+        end = None
+        try:
+            ending = r"\012} " + lig + r";\012\012"
+            end = calt_line.index(ending)
+        except ValueError as e:
+            try:
+                ending = r"\012} " + lig + r';";'
+                end = calt_line.index(ending)
+            except ValueError as e:
+                print(f"Error: could not find end: {e}")
+                exit(1)
+
+        print(f"  Removing {lig}")
+        calt_line = calt_line[:start] + calt_line[end:]
 
     print("\n Writing new glyphs file")
     with open("FiraCode.glyphs", "w") as nf:
-        lines[count] = calt_line
-        nf.seek(0)
-        for line in lines:
-            nf.write(line)
+        lines[line_num] = calt_line
+        nf.writelines(lines)
