8dc831b598ab3e12d23f03ddf02cfc09d74ac6b3
achuie <31836492+achuie@users.noreply.github.com>
2024-04-24 15:21:24 +0900

    Use C-CR to select completion.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -620,7 +620,7 @@ cmp.setup {
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete {},
-    ['<CR>'] = cmp.mapping.confirm {
+    ['<C-CR>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     },
