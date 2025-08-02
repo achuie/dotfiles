1a351a91ae5ee7977ec3c64f32e465afc4e6fc33
achuie <achuie@protonmail.com>
2025-08-02 22:03:46 +0900

    Updates for new sway system.

--- .config/sway/dyn_tags.sh
+++ .config/sway/dyn_tags.sh
@@ -7,3 +7,14 @@ JQ=$(command -v jq) || exit 2
 
 $SWAYMSG workspace $($SWAYMSG -t get_workspaces | $JQ -M '.[] | .name' \
     | tr -d '"' | sort -u | dmenu)
+
+MOVE=""
+MENUPROMPT="switch:"
+if [ $1 = "move" ]; then
+    MOVE="-t command move"
+    MENUPROMPT="move:"
+fi
+
+$SWAYMSG $MOVE workspace $($SWAYMSG -t get_workspaces | $JQ -M '.[] | .name' \
+    | tr -d '"' | sort -u | tofi -c $HOME/.config/tofi/tofi_run_theme --anchor=bottom \
+    --prompt-text=" $MENUPROMPT " --margin-bottom=18)
